8ea0d4377c38bb373891257644c3d59bbdb22da1
Pufferfish <tanzeeltariq39@gmail.com>
2021-06-15 00:08:58 +0500

    changes in htaccess

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use http\Url;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -25,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Url::forceScheme('https');
     }
 }
