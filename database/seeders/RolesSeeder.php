11a5dbbc83908050847bccf4136871233af46bca
Pufferfish <tanzeeltariq39@gmail.com>
2021-06-17 01:18:13 +0500

    db schema for roles

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Roles;
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Roles::create( ['name' => 'admin'] );
+        Roles::create( ['name' => 'user'] );
+    }
+}
