e515d9a519a6d76e2582c132e708c27f28819d75
Pufferfish <tanzeeltariq39@gmail.com>
2021-06-17 01:18:03 +0500

    db schema for roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
+//        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
