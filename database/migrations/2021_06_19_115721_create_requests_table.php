bcb24e7cc00fad0392f0891cb9c1e2c5453f7bbe
Pufferfish <tanzeeltariq39@gmail.com>
2021-06-19 17:00:51 +0500

    model and migrations

---
+++ database/migrations/2021_06_19_115721_create_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table -> string('title');
+            $table -> string('link');
+            $table -> string('sub_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
