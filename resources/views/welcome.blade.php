942373cecee06103e06da888e5bee12df48acb4c
Pufferfish <tanzeeltariq39@gmail.com>
2021-06-19 19:00:41 +0500

    added bootstrap carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,157 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="carousel relative mt-10  bg-base">
-        <div class="carousel-inner relative overflow-hidden w-full">
-            <!--Slide 1-->
-            <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden=""
-                   checked="checked">
-            <div class="carousel-item absolute opacity-0" style="height:auto;">
-                <div class="flex">
-                    <div class="flex flex-wrap -mx-1 lg:-mx-4">
-                        <div class="my-1 w-full p-20 md:w-1/2 lg:my-4 lg:w-1/3 ">
-                            <article class="overflow-hidden rounded-lg shadow-lg">
-
-                                <a href="#">
-                                    <img alt="Placeholder" class="block h-auto w-full"
-                                         src="https://picsum.photos/600/400/?random">
-                                </a>
-
-                                <header class="flex items-center justify-between leading-tight p-2 md:p-4">
-                                    <h1 class="text-lg">
-                                        <a class="no-underline hover:underline text-black" href="#">
-                                            Article Title
-                                        </a>
-                                    </h1>
-                                    <p class="text-grey-darker text-sm">
-                                        11/1/19
-                                    </p>
-                                </header>
-
-                                <footer class="flex items-center justify-between leading-none p-2 md:p-4">
-                                    <a class="flex items-center no-underline hover:underline text-black" href="#">
-                                        <img alt="Placeholder" class="block rounded-full"
-                                             src="https://picsum.photos/32/32/?random">
-                                        <p class="ml-2 text-sm">
-                                            Author Name
-                                        </p>
-                                    </a>
-                                    <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
-                                        <span class="hidden">Like</span>
-                                        <i class="fa fa-heart"></i>
-                                    </a>
-                                </footer>
-
-                            </article>
-                        </div>
-                        <div class="my-1 w-full p-20 md:w-1/2 lg:my-4 lg:w-1/3">
-                            <article class="overflow-hidden rounded-lg shadow-lg">
-
-                                <a href="#">
-                                    <img alt="Placeholder" class="block h-auto w-full"
-                                         src="https://picsum.photos/600/400/?random">
-                                </a>
-
-                                <header class="flex items-center justify-between leading-tight p-2 md:p-4">
-                                    <h1 class="text-lg">
-                                        <a class="no-underline hover:underline text-black" href="#">
-                                            Article Title
-                                        </a>
-                                    </h1>
-                                    <p class="text-grey-darker text-sm">
-                                        11/1/19
-                                    </p>
-                                </header>
-
-                                <footer class="flex items-center justify-between leading-none p-2 md:p-4">
-                                    <a class="flex items-center no-underline hover:underline text-black" href="#">
-                                        <img alt="Placeholder" class="block rounded-full"
-                                             src="https://picsum.photos/32/32/?random">
-                                        <p class="ml-2 text-sm">
-                                            Author Name
-                                        </p>
-                                    </a>
-                                    <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
-                                        <span class="hidden">Like</span>
-                                        <i class="fa fa-heart"></i>
-                                    </a>
-                                </footer>
-
-                            </article>
-                        </div>
-                        <div class="my-1 w-full p-20 md:w-1/2 lg:my-4 lg:w-1/3">
-                            <article class="overflow-hidden rounded-lg shadow-lg">
-
-                                <a href="#">
-                                    <img alt="Placeholder" class="block h-auto w-full"
-                                         src="https://picsum.photos/600/400/?random">
-                                </a>
-
-                                <header class="flex items-center justify-between leading-tight p-2 md:p-4">
-                                    <h1 class="text-lg">
-                                        <a class="no-underline hover:underline text-black" href="#">
-                                            Article Title
-                                        </a>
-                                    </h1>
-                                    <p class="text-grey-darker text-sm">
-                                        11/1/19
-                                    </p>
-                                </header>
-
-                                <footer class="flex items-center justify-between leading-none p-2 md:p-4">
-                                    <a class="flex items-center no-underline hover:underline text-black" href="#">
-                                        <img alt="Placeholder" class="block rounded-full"
-                                             src="https://picsum.photos/32/32/?random">
-                                        <p class="ml-2 text-sm">
-                                            Author Name
-                                        </p>
-                                    </a>
-                                    <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
-                                        <span class="hidden">Like</span>
-                                        <i class="fa fa-heart"></i>
-                                    </a>
-                                </footer>
-
-                            </article>
-                        </div>
-                    </div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            @foreach( $requests as $key => $request)
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="@if ($key == 0) active @endif"></li>
+            @endforeach
+        </ol>
+        <div class="carousel-inner">
+            @foreach ($requests as $key => $request)
+                <div class="carousel-item @if ($key == 0) active @endif">
+                    {{--                    <img class="d-block w-100" src="..." alt="First slide">--}}
+                    <iframe class="px-20 py-5 mb-8 d-block w-100" width="100%" height="515"
+                            src="{{ $request -> link }}?mute=1&autoplay=1">
+                    </iframe>
                 </div>
-            </div>
-            <label for="carousel-3"
-                   class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-            <label for="carousel-2"
-                   class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-            <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
-            <div class="carousel-item absolute opacity-0" style="height:50vh;">
-                <div class="block h-full w-full bg-orange-500 text-white text-5xl text-center">Slide 2</div>
-            </div>
-            <label for="carousel-1"
-                   class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-            <label for="carousel-3"
-                   class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-            <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
-            <div class="carousel-item absolute opacity-0" style="height:50vh;">
-                <div class="block h-full w-full bg-green-500 text-white text-5xl text-center">Slide 3</div>
-            </div>
-            <label for="carousel-2"
-                   class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-            <label for="carousel-1"
-                   class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-            <ol class="carousel-indicators">
-                <li class="inline-block mr-3">
-                    <label for="carousel-1"
-                           class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-                </li>
-                <li class="inline-block mr-3">
-                    <label for="carousel-2"
-                           class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-                </li>
-                <li class="inline-block mr-3">
-                    <label for="carousel-3"
-                           class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
-                </li>
-            </ol>
+            @endforeach
         </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
     <h2 class="text-center font-lato text-4xl font-semibold">About Mamaamo</h2>
     <div class="flex m-8">
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $requests = \App\Models\Request::take(3)-> orderBy('id','desc')->get();
+    return view('welcome' , compact('requests'));
 });
 
 Route::get('/dashboard', function () {
-    return view('welcome');
+    $requests = \App\Models\Request::take(3)-> orderBy('id','desc')->get();
+    return view('welcome' , compact('requests'));
 })->middleware(['auth'])->name('dashboard');
 
 //Admin Routes
