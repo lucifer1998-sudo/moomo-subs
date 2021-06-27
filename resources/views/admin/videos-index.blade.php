509eca9bad7a3ffb44a5d3b30f6b9be93ed1b610
Pufferfish <tanzeeltariq39@gmail.com>
2021-06-27 16:34:13 +0500

    videos listing

---
+++ resources/views/admin/videos-index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+<div class="mx-40">
+<div class="row">
+  <div class="col"><div class="p-3"><p class="h2">Videos</p></div></div>
+  <div class="col"><div class="p-3 text-right"><a href="{{route('admin.videos.create')}}" class="btn btn-dark">Add Videos</a></div></div>
+</div>
+<table class="table table-borderless mt-2 ">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Video Title</th>
+      <th scope="col">Video Link</th>
+    </tr>
+  </thead>
+  <tbody>
+        @foreach ($videos as $key => $video)
+            <tr>
+                <th scope="row">{{$key+1}}</th>
+                <td class="w-2/5">{{$video -> title}}</td>
+                <td class="w-2/5">{{$video -> link}}</td>
+            </tr>
+        @endforeach
+  </tbody>
+</table>
+</div>
+@endsection
