191b7187c54166b163b894e73eb72d5fb1e6c39e
Pufferfish <tanzeeltariq39@gmail.com>
2021-07-07 03:31:19 +0500

    music index video relation

--- app/Models/MusicIndex.php
+++ app/Models/MusicIndex.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\MusicIndexVideos;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MusicIndex extends Model
 {
     use HasFactory;
     protected $guarded = [];
     protected $table = 'music_index';
+
+    public function videos(){
+        return $this->hasManyThrough(Videos::class,MusicIndexVideos::class,'music_index_id','id','id','video_id');
+    }
 }
